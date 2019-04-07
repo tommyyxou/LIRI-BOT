549264159d627a92e7539eb2998922133b4004f0
tommyyxou <tommy.yx.ou@gmail.com>
2019-04-07 00:18:20 -0700

    Final

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ let keys = require("./keys.js");
 let spotify = new Spotify(keys.spotify);
 let axios = require("axios");
 let moment = require('moment');
+let fs = require("fs");
 // Extra Styling
 const chalk = require('chalk');
 
@@ -14,18 +15,30 @@ liri ();
 function liri () {
   if (verb === "concert-this") {
     let artist = noun;
+    let commandString = "\n\n" + verb + " " + noun
+    log (commandString);
+
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
     function(response) {
       for (i=0; i < response.data.length; i++ ) {
-      
-      let date = moment(response.data[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a");
+        
+        let date = moment(response.data[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-      console.log (chalk.blue("--------------------   Result: #" + (i+1) + "   ---------------------"));
-      console.log (chalk.green("Name of the venue: ") + response.data[i].venue.name);
-      console.log (chalk.green("Venue location: ") + response.data[i].venue.city + ", " + response.data[i].venue.region)
-      console.log (chalk.green("Date of the Event: ") + date);
-      console.log (chalk.blue("-----------------   End of Result: #" + (i+1) + "   -----------------"));
-      console.log (" ")
+        console.log (chalk.blue("--------------------   Result: #" + (i+1) + "   ---------------------"));
+        console.log (chalk.green("Name of the venue: ") + response.data[i].venue.name);
+        console.log (chalk.green("Venue location: ") + response.data[i].venue.city + ", " + response.data[i].venue.region)
+        console.log (chalk.green("Date of the Event: ") + date);
+        console.log (chalk.blue("-----------------   End of Result: #" + (i+1) + "   -----------------"));
+        console.log (" ")
+
+        let string = "\n\n--------------------   Result: #" + (i+1) + "   --------------------- " +
+        "\nName of the venue: " + response.data[i].venue.name + 
+        "\nVenue location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + 
+        "\nDate of the Event: " + date + 
+        "\n-----------------   End of Result: #" + (i+1) + "   -----------------";
+
+        log (string)
+        
       }
     });
   };
@@ -34,6 +47,9 @@ function liri () {
       if (noun === undefined) {
         song = "The Sign"
       } else {song = noun};
+
+      let commandString = "\n\n" + verb + " " + noun
+      log (commandString)
 
       spotify.search({ type: 'track', query: song }, function(err, data) {
         if (err) {
@@ -55,6 +71,15 @@ function liri () {
           console.log(chalk.green("Album Name: ") + data.tracks.items[x].album.name);
           console.log (chalk.red("-----------------   End of Result: #" + (x+1) + "   -----------------"));
           console.log (" ");
+
+          let string = "\n\n--------------------   Result: #" + (x+1) + "   ---------------------" +
+          "\nArtist(s): " + name + 
+          "\nTrack Name: " + song + 
+          "\nPreview: " + data.tracks.items[x].album.external_urls.spotify + 
+          "\nAlbum Name: " + data.tracks.items[x].album.name +
+          "\n-----------------   End of Result: #" + (x+1) + "   -----------------";
+
+          log (string);
         }
       });
   };
@@ -63,10 +88,13 @@ function liri () {
     if (noun === undefined) {
       movie = "Mr. Nobody"
     } else { movie = noun; };
+
+    let commandString = "\n\n" + verb + " " + noun;
+    log (commandString);
     
     axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
     function(response) {
-      console.log (chalk.yellow("--------------------   Result   ---------------------"));
+      console.log(chalk.yellow("--------------------   Result   ---------------------"));
       console.log(chalk.green("Title of the movie: ") + response.data.Title);
       console.log(chalk.green("Year the movie came out: ") + response.data.Year);
       console.log(chalk.green("IMDB Rating of the movie: ") + response.data.Ratings[0].Value);
@@ -75,13 +103,25 @@ function liri () {
       console.log(chalk.green("Language of the movie: ") + response.data.Language);
       console.log(chalk.green("Plot of the movie: ") + response.data.Plot);
       console.log(chalk.green("Actors in the movie: ") + response.data.Actors);
-      console.log (chalk.yellow("-----------------   End of Result   -----------------"));
+      console.log(chalk.yellow("-----------------   End of Result   -----------------"));
+
+      let string = "\n\n--------------------   Result   ---------------------" +
+      "\nTitle of the movie: " + response.data.Title +
+      "\nYear the movie came out: " + response.data.Year + 
+      "\nIMDB Rating of the movie: " + response.data.Ratings[0].Value + 
+      "\nRotten Tomatoes Rating of the moive: " + response.data.Ratings[1].Value +
+      "\nCountry where the moive was produced: " + response.data.Country + 
+      "\nLanguage of the movie: " + response.data.Language+ 
+      "\nPlot of the movie: " + response.data.Plot + 
+      "\nActors in the movie: " + response.data.Actors +
+      "\n-----------------   End of Result   -----------------";
+
+      log (string);
     });
   };
 }
 
 if (verb === "do-what-it-says") {
-  var fs = require("fs");
 
   fs.readFile("random.txt", "utf8", function(error, data) {
     if (error) {
@@ -95,3 +135,9 @@ if (verb === "do-what-it-says") {
     liri ();
   });
 };
+
+function log (string) {
+  fs.appendFile("log.txt", string, function(err) {
+    //console.log ("Error:", err)
+  });
+}
